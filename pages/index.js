3e5c3bc8849dcd846f9cb972cfdf8e2ea68be40f
Qiong Zhou Huang <qionghuang6@gmail.com>
2020-09-19 21:08:44 -0400

    Add ability to fetch data from opensky api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import Head from 'next/head'
+import {Typography} from '@material-ui/core';
 
 export default class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      coords: [0,0],
+      planes: [],
+      radius: 10,
+    }
+  }
   render(){
     return (
       <div>
         <Head>
           <title>AeroSeeker</title>
         </Head>
-        <h1>Hello World!</h1>
+        <Typography variant="h1">Hello World</Typography>
+        <Typography>{JSON.stringify(this.props.data)}</Typography>
       </div>
     )
+  }
+}
+
+export async function getStaticProps(){
+  const res = await fetch('https://opensky-network.org/api/states/all');
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
   }
 }
