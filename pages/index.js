f4ef7952b69593b07469fab69c7999d87d3d8b9f
Qiong Zhou Huang <qionghuang6@gmail.com>
2020-09-19 20:57:51 -0400

    Use create-next-app and remove unecessary files

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default class Home extends React.Component{
+  render(){
+    return (
+      <div>
+        <Head>
+          <title>AeroSeeker</title>
+        </Head>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
